7f84e10fb4823b67129f2ea866108b4e933b0267
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-06 13:45:59 -0500

    Choice change function created. Daytrip function created but needs refactoring.

--- main.py
+++ main.py
@@ -21,15 +21,46 @@ def random_entertainment(list):
     form = random.choice(list)
     return form
 
+
+def change_choice(elem, list):
+    list.remove(elem)
+    new_elem = random.choice(list)
+    check = input(f"Switch to {new_elem}? ")
+    if check == 'yes':
+        print('OK!')
+        return new_elem
+    else:
+        try:
+            change_choice(new_elem, list)
+        except:
+            print("I'm out of ideas!")
+
+# TODO: make daytrip generator that can be reused with random values or new chosen values
+
 def random_daytrip():
     destination = random_destination(destinations)
     restaurant = random_restaurant(restaurants)
     mode = random_transportation(transportation_modes)
     form = random_entertainment(entertainment_forms)
-    happy = input(f"Would you like go to {destination} by {mode} and eat at {restaurant} you {form}? ")
+    happy = input(f"Would you like go to {destination} by {mode} and eat at {restaurant} after you {form}? ")
     if happy == "yes":
         print("Yay!")
     else:
-        print("Oh no")
+        change_destination = input("Do you want to change the destination? ")
+        if change_destination == 'yes':
+            new_destination = change_choice(destination, destinations)
+        change_mode = input("Do you want to change the mode of transportation? ")
+        if change_mode == 'yes':
+            new_mode = change_choice(mode, transportation_modes)
+        change_restaurant = input("Do you want to change the restaurant? ")
+        if change_restaurant == 'yes':
+            new_restaurant = change_choice(restaurant, restaurants)
+        change_form = input("Do you want to change the form of entertainment? ")
+        if change_form == 'yes':
+            new_form = change_choice(form, entertainment_forms)
+
+
+    
+
 
 random_daytrip()
