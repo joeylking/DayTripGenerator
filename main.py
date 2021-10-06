f52e8210c7f0665f8a5c8835d34f4595b277dc18
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-06 11:08:29 -0500

    data added to lists

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+destinations = ["Fredericksburg", "Waco", "San Marcos", "San Antonio"]
+restaurants = ["Olive Garden", "Chili's", "Chipotle", "McDonald's"]
+transportation_modes = ["car", "horse", "uber", "run"]
+entertainment_forms = ["watch a movie", "play mini golf", "go to the park", "go antiquing"]
