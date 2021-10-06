da2a27f43c9ba250031aeea589edb51fd1a6ee7d
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-06 14:16:58 -0500

    Refactored daytrip function. Everything working as it should.

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ def random_entertainment(list):
     form = random.choice(list)
     return form
 
+r_destination = random_destination(destinations)
+r_restaurant = random_restaurant(restaurants)
+r_mode = random_transportation(transportation_modes)
+r_form = random_entertainment(entertainment_forms)
 
 def change_choice(elem, list):
     list.remove(elem)
@@ -35,32 +39,32 @@ def change_choice(elem, list):
         except:
             print("I'm out of ideas!")
 
-# TODO: make daytrip generator that can be reused with random values or new chosen values
-
-def random_daytrip():
-    destination = random_destination(destinations)
-    restaurant = random_restaurant(restaurants)
-    mode = random_transportation(transportation_modes)
-    form = random_entertainment(entertainment_forms)
-    happy = input(f"Would you like go to {destination} by {mode} and eat at {restaurant} after you {form}? ")
-    if happy == "yes":
-        print("Yay!")
+def daytrip_generator(destination, mode, restaurant, form):
+    confirm = input(f"Would you like go to {destination} by {mode} and eat at {restaurant} after you {form}? ")
+    if confirm == "yes":
+        print("Have fun!")
     else:
         change_destination = input("Do you want to change the destination? ")
         if change_destination == 'yes':
             new_destination = change_choice(destination, destinations)
+        else:
+            new_destination = destination
         change_mode = input("Do you want to change the mode of transportation? ")
         if change_mode == 'yes':
             new_mode = change_choice(mode, transportation_modes)
+        else:
+            new_mode = mode
         change_restaurant = input("Do you want to change the restaurant? ")
         if change_restaurant == 'yes':
             new_restaurant = change_choice(restaurant, restaurants)
+        else:
+            new_restaurant = restaurant
         change_form = input("Do you want to change the form of entertainment? ")
         if change_form == 'yes':
             new_form = change_choice(form, entertainment_forms)
+        else:
+            new_form = form
+        print("Great, let's make sure this is correct...")
+        daytrip_generator(new_destination, new_mode, new_restaurant, new_form)
 
-
-    
-
-
-random_daytrip()
+daytrip_generator(r_destination, r_mode, r_restaurant, r_form)
